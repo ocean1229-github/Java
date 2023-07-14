58eee1592b51461eda0d01d818d7ec8608c87c79
DoohyenKang <ocean1229@naver.com>
2023-07-14 22:51:59 +0900

    주석문

--- first/src/test/HelloWorld.java
+++ first/src/test/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
 //주석문 *Ctrl + /
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("test1");
+		System.out.println("test2");
+//		System.out.println("test3");
+		System.out.println("test4");
+/*		System.out.println("test5");
+		System.out.println("test6");*/
 	}
 
 }
