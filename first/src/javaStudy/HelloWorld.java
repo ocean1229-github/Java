be2588a064822109e316a7ca51dd32bfd1c82714
DoohyenKang <ocean1229@naver.com>
2023-07-15 23:02:44 +0900

    변수

--- first/src/javaStudy/HelloWorld.java
+++ first/src/javaStudy/HelloWorld.java
@@ -1,7 +1,10 @@
 package test;
-
+//이 클래스를 누가 만들었는지
 public class HelloWorld {
-//주석문 *Ctrl + /
+	/**
+	 * 
+	 * @param args 여기 들어있는 파라미터가 뭔지 설명하기 (문서화 주석)
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("test1");
@@ -10,6 +13,7 @@ public class HelloWorld {
 		System.out.println("test4");
 /*		System.out.println("test5");
 		System.out.println("test6");*/
+		System.out.println("주석 단축키 : Ctrl + /");
 	}
 
 }
