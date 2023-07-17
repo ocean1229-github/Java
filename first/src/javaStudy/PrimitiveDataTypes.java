2741ce45cee65903e586cb549104294084d794e1
DoohyenKang <ocean1229@naver.com>
2023-07-17 22:40:48 +0900

    기본형 타입

---
+++ first/src/javaStudy/PrimitiveDataTypes.java
@@ -0,0 +1,35 @@
+package javaStudy;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//리터럴은 일종의 값
+		// 정수리터럴 실수리터럴 문자리터럴 논리리터럴 이 존재한다.
+		// 특정타입변수에 대입가능
+		
+		// 논리형
+		boolean isFun = true;
+		System.out.println(isFun);
+		//true false
+		
+		// 문자형
+		// 항상 작은 따옴표 이용
+		// 문자 하나만 표현 가능
+		char c = 'f';
+		
+		// 실수리터럴
+		int x= 59;
+		
+		// 정수리터럴
+		// long 타입의 경우에는 숫자 마지막에 항상 l 혹은 L을 대입한다.
+		long bing = 345455433l;
+		
+		// float 타입의 경우에는 숫자 마지막에 항상 f 혹은 F를 대입
+		float f = 32.4f;
+		
+		double d = 3434343.5;
+		
+	}
+
+}
