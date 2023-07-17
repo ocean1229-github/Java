a494cacab454631fb4ea9dbad7a08adec6af093a
DoohyenKang <ocean1229@naver.com>
2023-07-17 22:42:17 +0900

    기본형 타입 - 문제

--- first/src/javaStudy/PrimitiveDataTypes.java
+++ first/src/javaStudy/PrimitiveDataTypes.java
@@ -29,6 +29,24 @@ public class PrimitiveDataTypes {
 		float f = 32.4f;
 		
 		double d = 3434343.5;
+
+		// 기본형 타입 문제   
+		boolean
+		 isFun2 = false;
+		        
+		char
+		 charValue2 = 'a';
+		        
+		int
+		 intValue2 = 20;
+		        
+		long
+		 longValue2 = 2147483648L; 
+
+		        System.out.println(isFun2);
+		        System.out.println(charValue2);
+		        System.out.println(intValue2);
+		        System.out.println(longValue2);
 		
 	}
 
