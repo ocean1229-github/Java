fbae170fe8c48c296c021fc76719bfd7c50a86eb
DoohyenKang <ocean1229@naver.com>
2023-07-20 23:19:39 +0900

    삼항연산자 문제

--- first/src/javaStudy/OperatorExam5.java
+++ first/src/javaStudy/OperatorExam5.java
@@ -1,4 +1,5 @@
 package javaStudy;
+import java.util.Calendar;
 
 public class OperatorExam5 {
 
@@ -19,6 +20,16 @@ public class OperatorExam5 {
 		}
 		
 		System.out.println(b2);
+		
+		// 문제
+		int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        String ampm;
+        // 삼항연산자를 이용해서 ampm의 값을 "오전" 또는 "오후"로 만들어보세요.
+        ampm = 
+hour < 12 ? "오전" : "오후";
+
+        System.out.println("지금시간은 " + hour + "시이고, " + ampm + "입니다.");
+		
 	}
 
 }
