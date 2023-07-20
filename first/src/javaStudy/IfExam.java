9c085890e1789adfca7db290459b1f92048544b3
DoohyenKang <ocean1229@naver.com>
2023-07-20 22:54:48 +0900

    if - 문제 2

--- first/src/javaStudy/IfExam.java
+++ first/src/javaStudy/IfExam.java
@@ -29,6 +29,17 @@ public class IfExam {
 		if(value % 3 == 0) {
 			System.out.println("value는 3의 배수입니다.");
 		}
+		
+		// 문제2
+		// 변수 value가 선언되어 있다고 가정하고 아래에 코드를 작성하세요.
+        int ret = 0;
+        if( value % 3 == 0 ){
+            ret = 3;
+        }// 이 아래 줄에 else 구문을 추가해서 코드를 완성하세요.
+        else if(value % 4 == 0){
+            ret = 4;
+        }
+
 	}
 
 }
