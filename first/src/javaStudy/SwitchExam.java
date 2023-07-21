26b17399092f267c13941e2785ba65f26479c901
DoohyenKang <ocean1229@naver.com>
2023-07-21 23:13:43 +0900

    switch문 - 문제 꿀팁

--- first/src/javaStudy/SwitchExam.java
+++ first/src/javaStudy/SwitchExam.java
@@ -67,9 +67,31 @@ public class SwitchExam {
                 break;
             case 12:
                 season = "겨울";
-                break;               
-                
-            
+                break;                  
+        }
+        
+     // 다음과 같이 case문을 한번에 사용하면 더 짧게 코드를 짤 수 있습니다.
+        switch(month) {
+            case 1:
+            case 2:
+            case 12:
+                season = "겨울";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "봄";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "여름";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "가을";
+                break;
         }
         
         System.out.println("지금은 " + month + "월이고, " + season + "입니다.");
