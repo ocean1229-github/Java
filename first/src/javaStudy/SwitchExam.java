f69cafd473c546f3270cfd6caa9f2816b56f55fe
DoohyenKang <ocean1229@naver.com>
2023-07-21 22:58:30 +0900

    switch문

---
+++ first/src/javaStudy/SwitchExam.java
@@ -0,0 +1,30 @@
+package javaStudy;
+
+public class SwitchExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// switch, case, default, break
+		
+		int value = 1;
+		
+		switch(value) {
+			case 1:
+				System.out.println("1");
+			case 2:
+				System.out.println("2");
+			case 3:
+				System.out.println("3");
+			default :
+				System.out.println("그 외 다른 숫자");
+		}
+		String str = "A";
+		switch(str) {
+		case "A":
+			System.out.println("A");
+			break;
+		case "B":
+		}
+	}
+
+}
