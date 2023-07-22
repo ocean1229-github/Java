2ac65d3c41f1096a15c6faf7dd8b362ec02fe99f
DoohyenKang <ocean1229@naver.com>
2023-07-22 22:12:10 +0900

    while 문제

--- first/src/javaStudy/WhileExam.java
+++ first/src/javaStudy/WhileExam.java
@@ -3,24 +3,35 @@ package javaStudy;
 public class WhileExam {
 	
 	public static void main(String [] args) {
-		int i = 0;
+//		int i = 0;
+//		
+//		while(i<10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		
+//		int total = 0;
+//		int j = 1;
+//		while(j<=100) {
+//			total = total + j;
+//			j++;
+//		}
+//		System.out.println(total);
+//		
+//		while(true) {
+//			System.out.println("hello");
+//		}
+//		
+		// 문제
+		int i = 1;
+        while(i < 11){
+            // if 문을 추가해, i가 짝수일때만 i를 출력하는 코드를 짜보세요.
+            if(i%2==0){
+                System.out.println(i);   
+            }
+            i++; 
+        }
 		
-		while(i<10) {
-			System.out.println(i);
-			i++;
-		}
-		
-		int total = 0;
-		int j = 1;
-		while(j<=100) {
-			total = total + j;
-			j++;
-		}
-		System.out.println(total);
-		
-		while(true) {
-			System.out.println("hello");
-		}
 		
 	}
 }
