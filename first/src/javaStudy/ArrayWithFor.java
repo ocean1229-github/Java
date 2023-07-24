5c6d45c1aa660ed7d773cfb270f8b2beaee7db53
DoohyenKang <ocean1229@naver.com>
2023-07-24 22:14:32 +0900

    배열 사용하기

---
+++ first/src/javaStudy/ArrayWithFor.java
@@ -0,0 +1,28 @@
+package javaStudy;
+
+public class ArrayWithFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] iarray = new int[100];
+		iarray[0] = 1;
+		iarray[1] = 2;
+		
+//			System.out.println(iarray.length);
+		for(int i = 0; i < iarray.length; i++) {
+			iarray[i] = i + 1;
+		
+		}
+		
+		int sum = 0;
+		for(int i = 0; i< iarray.length; i++) {
+			//변수의 Scope (i는 for문에 생성되었다가 for문이 끝나면 없어진다)
+			sum = sum + iarray[i];
+			
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
