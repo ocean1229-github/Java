fbd8d1a7666954e3536e60ad1d731da9d1ff4d91
DoohyenKang <ocean1229@naver.com>
2023-07-24 22:41:35 +0900

    forEach - 문제

--- first/src/javaStudy/forEachExam.java
+++ first/src/javaStudy/forEachExam.java
@@ -20,6 +20,13 @@ public class forEachExam {
 			//40
 			//50
 		}
+		
+		//문제
+		int [] array = {1, 5, 3, 6, 7};
+        //for each문을 이용해서 array의 값을 한 줄씩 출력하세요
+        for (int value:array){
+            System.out.println(value);
+        }
 	}
 
 }
