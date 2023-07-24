1cc4744674681f4b42e08c17c22aac1984ae6ede
DoohyenKang <ocean1229@naver.com>
2023-07-24 23:35:07 +0900

    Class

---
+++ first/src/javaStudy/CarExam.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1 = new Car();
+		Car c2 = new Car();
+//		new 연산자는 new연산자 뒤에 나오는 생성자를 이용하여 메모리에 객체를 만들라는 명령.
+//		메모리에 만들어진 객체를 인스턴스(instance)라고도 한다.
+//		이렇게 만들어진 객체를 참조하는 변수가 c1 , c2 이다.
+//		위의 코드가 실행되면 Car라는 객체가 2개가 만들어지고 각각의 객체를 참조하는 c1과 c2변수가 선언됩니다.
+	}
+
+}
