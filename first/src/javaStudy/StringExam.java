c243eff0a087cdce4ca6bad4f9c74f4b54bf2afa
DoohyenKang <ocean1229@naver.com>
2023-07-25 23:10:51 +0900

    String클래스 - 문제

--- first/src/javaStudy/StringExam.java
+++ first/src/javaStudy/StringExam.java
@@ -28,6 +28,23 @@ public class StringExam {
 		System.out.println(str1.substring(3));
 		
 		System.out.println(str1);
+	
+	
+		// 문제
+//		String str1 = new String("Hello world");
+//        String str2 = new String("Hello world");
+//        
+//        if( str1.equals(str2) ){
+//            System.out.println("str1과 str2는 같은 값을 가지고 있습니다.");
+//        }
+//        else{
+//            System.out.println("str1과 str2는 다른 값을 가지고 있습니다.");
+//        }
+		// str1과 str2는 같은 값을 가지고 있습니다.ㅌ
+		
+	
 	}
 
+	
+	
 }
