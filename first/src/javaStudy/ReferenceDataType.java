e50d7efc7022b99ca7753eee68d586f006f734c5
DoohyenKang <ocean1229@naver.com>
2023-07-25 22:22:32 +0900

    참조타입

---
+++ first/src/javaStudy/ReferenceDataType.java
@@ -0,0 +1,19 @@
+package javaStudy;
+
+public class ReferenceDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 4;
+		
+		// 참조형 변수
+		String str = new String("hello");
+		// String 참조형 타입
+		// new (생성자) 클래스를 메모리에 올려주세요
+		// 메모리에 올라간 클래스를 인스턴스
+		// 레퍼런스(참조)한다.
+		// 즉 str은 이 String("hello")를 참조한다. (가르킨다)
+		
+	}
+
+}
