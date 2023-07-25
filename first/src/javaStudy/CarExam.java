c40ff109ad584045318e06b17b6d5b1870880a76
DoohyenKang <ocean1229@naver.com>
2023-07-25 23:16:24 +0900

    필드

--- first/src/javaStudy/CarExam.java
+++ first/src/javaStudy/CarExam.java
@@ -10,6 +10,20 @@ public class CarExam {
 //		메모리에 만들어진 객체를 인스턴스(instance)라고도 한다.
 //		이렇게 만들어진 객체를 참조하는 변수가 c1 , c2 이다.
 //		위의 코드가 실행되면 Car라는 객체가 2개가 만들어지고 각각의 객체를 참조하는 c1과 c2변수가 선언됩니다.
+	
+		//객체별로 name 과 number가짐
+		c1.name = "소나타";
+		c1.number = 1234;
+		
+		c2.name = "구급차";
+		c2.number = 1111;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
+		
+		System.out.println(c2.name);
+		System.out.println(c2.number);
+		
 	}
 
 }
