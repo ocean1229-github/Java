c1e6a05bbe82c051b67e15725551e55430fc67ba
DoohyenKang <ocean1229@naver.com>
2023-07-31 23:08:02 +0900

    String 클래스 메소드 문제

--- first/src/javaStudy/StringMethodExam.java
+++ first/src/javaStudy/StringMethodExam.java
@@ -14,5 +14,26 @@ public class StringMethodExam {
 		
 		System.out.println(str.substring(3));
 		System.out.println(str.substring(3, 6));
+	
+	
+		//문제
+		 String str1 = "안녕하세요. ";
+	        String str2 = "벌써 여기까지 오셨네요. 끝까지 화이팅!!";
+	        
+	        String concatResult;
+	        String substringResult;
+	        
+	        // 아래쪽에 코드를 작성하세요.
+	        concatResult = str1.concat(str2);
+	        
+	        substringResult = str1.substring(2);
+	        
+	        
+	        
+	        // 이 아래는 정답 확인을 위한 코드입니다. 수정하지 마세요.
+	        System.out.println(concatResult);
+	        System.out.println(substringResult);
+	    
+	
 	}
 }
