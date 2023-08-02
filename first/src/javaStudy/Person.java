ac8ffbb408ac63502f525fb6d6240a9370808e77
DoohyenKang <ocean1229@naver.com>
2023-08-02 22:23:05 +0900

    생성자 - 문제

---
+++ first/src/javaStudy/Person.java
@@ -0,0 +1,17 @@
+package javaStudy;
+
+//※ 본 문제는 두 코드 파일, Person.java와 PersonExam.java로 구성되어있습니다.
+//파일 이름을 클릭하면 파일 내용물을 볼 수 있습니다.
+//
+//Person클래스에 int형 변수를 매개변수로 받아 age 값을 초기화하는 생성자를 만들어주세요.
+
+public class Person {
+	int age;
+	
+	Person(int number){
+		age = number;
+	}
+//	생성자는 객체 생성 시 객체의 필드를 초기화합니다.
+//	다음 코드에서 Car 생성자는 문자열을 매개변수로 받아,
+//	자동차의 이름을 짓습니다
+}
---
+++ first/src/javaStudy/PersonExam.java
@@ -0,0 +1,8 @@
+package javaStudy;
+
+public class PersonExam {
+    public static void main(String[] args) {
+        //Person클래스에서 int형 변수를 매개변수로 받는 생성자를 호출합니다.
+        Person person = new Person(25);
+    }
+}
