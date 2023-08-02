4dcac9d72f87eacf07a377713a0919b84e2d0cf0
DoohyenKang <ocean1229@naver.com>
2023-08-02 22:36:15 +0900

    this 키워드

--- first/src/javaStudy/Car.java
+++ first/src/javaStudy/Car.java
@@ -9,11 +9,15 @@ public class Car {
 	}
 	// void 내용은 class 안에서만 가능.
 	
-	public Car(String n) {
-		name = n;
+	public Car(String name) {
+		this.name = name;
 	}
 	// 사용자가 생성자를 하나라도 추가했더니
 	// 기본 생성자는 자동으로 만들어지지 않았더라.
+	
+	// this - 객체 자신을 참조하는 키워드
+	
+	
 }
 
 
