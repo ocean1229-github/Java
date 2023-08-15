98ab95bf3becf8810709d4aa660e5c8a6d26b1b6
강두현 <ocean1229@naver.com>
2023-08-15 13:17:18 +0000

    백준 10998번

---
+++ first/src/B aekJoon/ex1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2;
+		
+		Scanner s = new Scanner(System.in);
+		num1 = s.nextInt();
+		num2 = s.nextInt();
+
+		System.out.println(num1*num2);
+	}
+
+}
