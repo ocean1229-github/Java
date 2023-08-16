7c7e599dc975420e19fa807d8a680161582f654b
강두현 <ocean1229@naver.com>
2023-08-16 13:26:18 +0000

    10926 브론즈5 문자열 합치기

---
+++ first/src/B aekJoon/ex4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "??!";
+		
+		Scanner s = new Scanner(System.in);
+		String word = s.nextLine();
+
+		System.out.println(word + str);
+	}
+
+}
